00132becb1f8f92ebc8cfa7077fbf96763099404
MalikZain1014 <malikzain@gmail.com>
2024-03-13 21:07:17 +0500

    .

--- vite.config.js.timestamp-1690314749713-2e85c33cd10f5.mjs
+++ vite.config.js.timestamp-1690314749713-2e85c33cd10f5.mjs
@@ -1,10 +1,10 @@
-// // vite.config.js.timestamp-1690314749713-2e85c33cd10f5.mjs
-// import { defineConfig } from "file:///C:/Users/Home/Desktop/react-fitness-app/node_modules/vite/dist/node/index.js";
-// import react from "file:///C:/Users/Home/Desktop/react-fitness-app/node_modules/@vitejs/plugin-react/dist/index.mjs";
+// vite.config.js.timestamp-1690314749713-2e85c33cd10f5.mjs
+import { defineConfig } from "file:///C:/Users/Home/Desktop/react-fitness-app/node_modules/vite/dist/node/index.js";
+import react from "file:///C:/Users/Home/Desktop/react-fitness-app/node_modules/@vitejs/plugin-react/dist/index.mjs";
 
-// const viteConfig = defineConfig({
-//   base: "/react-fitness-app/",
-//   plugins: [react()],
-// });
+const viteConfig = defineConfig({
+  base: "/react-fitness-app/",
+  plugins: [react()],
+});
 
-// export default viteConfig;
+export default viteConfig;
